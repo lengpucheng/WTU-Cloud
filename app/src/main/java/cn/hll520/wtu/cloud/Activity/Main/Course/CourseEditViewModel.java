f6c1d0e9a03d1110d8867f1d2d46e62acd1b600d
lengpucheng <lpc@hll520.cn>
2020-04-28 17:39:26 +0800

    添加了编辑课程的页面

---
+++ app/src/main/java/cn/hll520/wtu/cloud/Activity/Main/Course/CourseEditViewModel.java
@@ -0,0 +1,7 @@
+package cn.hll520.wtu.cloud.Activity.Main.Course;
+
+import androidx.lifecycle.ViewModel;
+
+public class CourseEditViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
