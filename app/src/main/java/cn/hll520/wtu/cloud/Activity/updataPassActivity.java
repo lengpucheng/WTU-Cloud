de9e5eb80ee3c87111cd39c1397d410f98c33441
lengpucheng <lpc@hll520.cn>
2020-04-28 15:04:08 +0800

    添加：修改密码的界面

---
+++ app/src/main/java/cn/hll520/wtu/cloud/Activity/updataPassActivity.java
@@ -0,0 +1,16 @@
+package cn.hll520.wtu.cloud.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.hll520.wtu.cloud.R;
+
+public class updataPassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updata_pass);
+    }
+}
