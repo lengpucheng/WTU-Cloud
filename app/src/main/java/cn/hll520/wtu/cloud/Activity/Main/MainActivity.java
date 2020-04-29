dd9b6405e03b739ae34a2546213bea0ac541ae7f
lengpucheng <lengpucheng@qq.com>
2020-04-29 20:38:26 +0800

    修复了启动页面不关闭的BUG

--- app/src/main/java/cn/hll520/wtu/cloud/Activity/Main/MainActivity.java
+++ app/src/main/java/cn/hll520/wtu/cloud/Activity/Main/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
@@ -19,18 +20,30 @@ import cn.hll520.wtu.cloud.R;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView menu=findViewById(R.id.mainMenubottom);
+        //按钮
+        ImageView menubar=findViewById(R.id.mainAdd);
+        //工具栏
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        //绑定工具栏
+        setSupportActionBar(toolbar);
+        //打开工具栏
+        menubar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openOptionsMenu();
+            }
+        });
+
+
         //获取导航控制
         NavController controller= Navigation.findNavController(this,R.id.mainfragment);
         //绑定底部菜单与导航
         NavigationUI.setupWithNavController(menu,controller);
-        //配置底部导航   用导航组来构建一个工具条
-//        AppBarConfiguration configuration=new AppBarConfiguration.Builder(menu.getMenu()).build();
         ImageView imageView=findViewById(R.id.mainUserIMG);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
